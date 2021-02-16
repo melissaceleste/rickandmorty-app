89306c6a465a855d325f071087e51117eb1380aa
Melissa Celeste Gries <m.c.gries@gmx.de>
2021-02-16 21:22:54 +0100

    add some more features

--- src/App.js
+++ src/App.js
@@ -2,18 +2,50 @@ import data from './rickandmortyapi.json'
 import Card from './components/Card'
 import './styles/_base.css'
 
+function Header() {
+  return <header className="Header"> RICK & MORTY</header>
+}
+
+function Navigation({ onNavigate }) {
+  return (
+    <nav className="Navigation">
+      <button className="Navigation__button" onClick={() => onNavigate('one')}>
+        1
+      </button>
+      <button className="Navigation__button" onClick={() => onNavigate('two')}>
+        2
+      </button>
+      <button
+        className="Navigation__button"
+        onClick={() => onNavigate('three')}
+      >
+        3
+      </button>
+      <button className="Navigation__button" onClick={() => onNavigate('four')}>
+        4
+      </button>
+    </nav>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      hello world
+      <Header />
+
       {data.results.map(character => (
         <Card
           key={character.id}
           name={character.name}
           species={character.species}
           image={character.image}
+          status={character.status}
+          gender={character.gender}
+          origin={character.origin.name}
         />
       ))}
+
+      <Navigation />
     </div>
   )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,53 @@
 import '../Card/Card.css'
+import React, { useState } from 'react'
 
-export default function Card({ name, species, image }) {
+export default function Card({ name, species, image, status, gender, origin }) {
+  const [isTextVisible, setIsTextVisible] = useState(false)
+
+  const Human = (
+    <img
+      src="../../../public/human.png"
+      width="50px"
+      height="50px"
+      alt="human"
+    />
+  )
+  const Alien = (
+    <img
+      src="../../../public/alien.png"
+      width="50px"
+      height="50px"
+      alt="alien"
+    />
+  )
+
   return (
     <section className="Card">
       <strong> {name} </strong>
-      {species === 'Human' ? '👩🏽' : '🧟‍♀️'}
+      {species === 'Human' ? Human : Alien}
       <p>{<img className="img" src={image} alt="" width="200px" />}</p>
+
+      <button
+        className="button"
+        onClick={event => {
+          event.stopPropagation()
+          setIsTextVisible(!isTextVisible)
+        }}
+      >
+        <img
+          src="../../../public/button_down.svg"
+          width="10px"
+          height="10px"
+          alt="show more information"
+        />
+      </button>
+      <section hidden={!isTextVisible}>
+        <ul className="ul">
+          <li>status: {status}</li>
+          <li> gender: {gender} </li>
+          <li> origin: {origin} </li>
+        </ul>
+      </section>
     </section>
   )
 }
