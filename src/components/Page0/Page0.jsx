73251f5e6cb32a44dd09114498d32b650a223304
Melissa Celeste Gries <m.c.gries@gmx.de>
2021-02-20 22:08:04 +0100

    style like-components

--- src/components/Page0/Page0.jsx
+++ src/components/Page0/Page0.jsx
@@ -8,12 +8,42 @@ export default function Page0({ hidden, currywurst }) {
 
   return (
     <section hidden={hidden} className="Page0">
+      <div class="buttons">
+        <button className="button" class="blob-btn">
+          show me the most voted
+          <span class="blob-btn__inner">
+            <span class="blob-btn__blobs">
+              <span class="blob-btn__blob"></span>
+              <span class="blob-btn__blob"></span>
+              <span class="blob-btn__blob"></span>
+              <span class="blob-btn__blob"></span>
+            </span>
+          </span>
+        </button>
+        <br />
+
+        <svg className="svg" xmlns="http://www.w3.org/2000/svg" version="1.1">
+          <defs>
+            <filter id="goo">
+              <feGaussianBlur
+                in="SourceGraphic"
+                result="blur"
+                stdDeviation="10"
+              ></feGaussianBlur>
+              <feColorMatrix
+                in="blur"
+                mode="matrix"
+                values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 21 -7"
+                result="goo"
+              ></feColorMatrix>
+              <feBlend in2="goo" in="SourceGraphic" result="mix"></feBlend>
+            </filter>
+          </defs>
+        </svg>
+      </div>
+
       {currywurst.map(character => (
-        <SmallCard
-          likeCharacter={character.isLiked}
-          image={character.image}
-          name={character.name}
-        />
+        <SmallCard image={character.image} name={character.name} />
       ))}
     </section>
   )
--- src/components/SmallCard/SmallCard.jsx
+++ src/components/SmallCard/SmallCard.jsx
@@ -1,22 +1,36 @@
 import '../SmallCard/SmallCard.css'
 import React, { useState } from 'react'
+import upvotessrc from '../../images/upvote.svg'
+import downvotessrc from '../../images/downvote.svg'
 
 export default function SmallCard({ name, image }) {
-  const [count, setCount] = useState(0)
+  const [countUpVotes, setCountUpVotes] = useState(0)
+  const [countDownVotes, setCountDownVotes] = useState(0)
 
   return (
     <>
       <figure>
         <img src={image} alt={name} width="100px" />
-        <figcaption className="figCaption">{name}</figcaption>
-      </figure>
+        <figcaption className="figCaption">
+          <section className="votingContainer">
+            <img
+              src={upvotessrc}
+              alt=""
+              className="upvoting"
+              onClick={() => setCountUpVotes(countUpVotes + 1)}
+            />
+            <p className="relative"> {countUpVotes} </p>
 
-      <button className="upvoting" onClick={() => setCount(count + 1)}>
-        {count}
-      </button>
-      <button className="downvoting" onClick={() => setCount(count + 1)}>
-        {count}
-      </button>
+            <img
+              src={downvotessrc}
+              alt=""
+              className="downvoting"
+              onClick={() => setCountDownVotes(countDownVotes + 1)}
+            />
+            <p className="relative"> {countDownVotes}</p>
+          </section>
+        </figcaption>
+      </figure>
     </>
   )
 }
